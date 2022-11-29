ecac1429b100dbfa6ce1d200a2ab253a3ec31a47
Yacinesaf <safyacine96@gmail.com>
2022-11-28 21:49:35 -0500

    Added modal to create a new budget

--- src/components/Budgets.vue
+++ src/components/Budgets.vue
@@ -1,26 +1,58 @@
 <template>
-  <div class="pa-4">
-    <div class="text-h3 text-center py-6" style="font-weight: 700">Your spending sheets</div>
-    <v-row class="pa-6" v-if="isLoadingBudgets">
+  <div class="pa-6">
+    <div v-if="data" class="text-h3 text-center py-6" style="font-weight: 700">Your spending sheets</div>
+    <v-row v-if="isLoadingBudgets">
       <v-col :key="i" v-for="i in Array.from(Array(8).keys())" cols="12" md="6" lg="3" xl="2">
         <v-skeleton-loader width="100%" v-bind="attrs" type="image"></v-skeleton-loader>
       </v-col>
     </v-row>
-    <v-row v-else style="height: calc(100vh - 116px)" align="center" justify="center">
+    <v-row class="pt-6" v-else align="center" justify="center">
       <v-row v-if="data" class="ma-0">
         <v-col :key="i" v-for="(budget, i) in data" cols="12" md="6" lg="3" xl="2">
           <budget-card :img="budget.img" :name="budget.name" :description="budget.description" :date="budget.date" />
         </v-col>
-        <v-col cols="12" md="6" lg="3" xl="2">
-          <v-btn style="width: 100%; min-height: 378px" class="rounded-lg" @click="addBudget" outlined
-            ><v-icon size="70">mdi-plus</v-icon>
-          </v-btn>
+        <v-col v-if="data" cols="12" md="6" lg="3" xl="2">
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-on="on"
+                v-bind="attrs"
+                style="width: 100%; min-height: 378px"
+                class="rounded-lg"
+                @click="addBudget"
+                outlined
+                ><v-icon size="70">mdi-plus</v-icon>
+              </v-btn>
+            </template>
+            <v-form>
+              <v-text-field
+                :rules="isLoggingIn ? [rules.required] : [rules.required, rules.email]"
+                style="border-radius: 6px"
+                label="Budget name"
+                v-model="BudgetName"
+                outlined
+              ></v-text-field>
+              
+            </v-form>
+          </v-dialog>
         </v-col>
       </v-row>
-      <v-col v-else class="d-flex align-center" style="flex-direction: column" cols="5">
+      <div
+        style="
+          position: absolute;
+          flex-direction: column;
+          top: 0;
+          bottom: 0;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        "
+        v-else
+        cols="5"
+      >
         <img src="@/assets/emptyState.png" height="200px" width="auto" wid alt="" />
         <div class="text-h6 text-md-h4 text-center mt-6">You don't have any budget set up</div>
-      </v-col>
+      </div>
     </v-row>
   </div>
 </template>
