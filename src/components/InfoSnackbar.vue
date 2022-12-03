97560a662d2e4e7b2912f90fee9f78538b0736e8
Yacinesaf <safyacine96@gmail.com>
2022-12-02 21:40:04 -0500

    Changed infos alert deisgn

--- src/components/InfoSnackbar.vue
+++ src/components/InfoSnackbar.vue
@@ -1,8 +1,19 @@
 <template>
-  <v-snackbar max-width="270px" timeout="-1" color="orange" bottom right :value="isInfoSnackbarShown" vertical>
-    Go to settings to finish your registration and get the full experience of the app
+  <v-snackbar
+    timeout="-1"
+    color="primary"
+    outlined
+    bottom
+    right
+    :value="isInfoSnackbarShown"
+    vertical
+  >
+    <div style="font-size: 1.15rem">
+      Go to settings to finish your registration and get the full experience of the app
+    </div>
     <template v-slot:action="{ attrs }">
-      <v-btn  text v-bind="attrs" @click="closeInfoSnackbar"> Close </v-btn>
+      <v-btn color="primary" text v-bind="attrs" @click="closeInfoSnackbar"> Do not show again </v-btn>
+      <v-btn color="primary" text v-bind="attrs" @click="goToSettings"> Go to settings </v-btn>
     </template>
   </v-snackbar>
 </template>
@@ -18,6 +29,9 @@ export default {
   methods: {
     closeInfoSnackbar() {
       this.$store.commit("general/closeMoreInfoSnackbar");
+    },
+    goToSettings() {
+      this.$router.push("/settings");
     },
   },
 };
