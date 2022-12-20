0142461663977874efea521431833f6bd5bddab1
Yacinesaf <safyacine96@gmail.com>
2022-12-19 23:59:46 -0500

    Added consition to route guarding

--- src/router/index.js
+++ src/router/index.js
@@ -51,11 +51,17 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
+  console.log("🚀  router.beforeEach  to", to)
+  console.log(firebase.auth().currentUser)
   // Check for requireAuth guard AND not logged in user
-  if (to.matched.some(record => record.meta.requiresAuth) && !firebase.auth().currentUser) next({ name: 'entry', params: { state: "login" } })
+  if (to.matched.some(record => { console.log(record.meta.requiresAuth); return record.meta.requiresAuth }) && !firebase.auth().currentUser) next({ name: 'EntryPage', params: { state: "login" } })
 
   // Check for requireGuest guard AND logged in user
-  else if (to.matched.some(record => record.meta.requireGuest) && firebase.auth().currentUser) next({ name: 'Budgets' })
+  else if (to.matched.some(record => { console.log(record.meta.requireGuest); return record.meta.requireGuest }) && firebase.auth().currentUser) {
+    if (from.path !== "/budgets") {
+      next({ name: 'Budgets' })
+    }
+  }
 
   else next()
   window.scrollTo(0, 0)
